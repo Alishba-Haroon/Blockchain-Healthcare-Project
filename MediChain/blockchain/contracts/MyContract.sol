71f32ea639828e644644b8d7e783cc6dd1492ae6
Alishba Haroon <156108243+Alishba-Haroon@users.noreply.github.com>
2025-10-30 10:58:33 +0500

    Update MyContract.sol

--- MediChain/blockchain/contracts/MyContract.sol
+++ MediChain/blockchain/contracts/MyContract.sol
@@ -3,12 +3,9 @@ pragma solidity ^0.8.20;
 
 contract MyContract {
     string public message;
-
     constructor(string memory _message) {
         message = _message;
     }
-
     function updateMessage(string memory _newMessage) public {
         message = _newMessage;
-    }
-}
+    }}
